97f0b142e6dd31329e96c45b325790c8979457b1
Tahir Iqbal <tahiriqbaldeveloper@gmail.com>
2023-09-23 02:35:37 +0200

    account service

--- src/main/java/com/brotech/Auth/repo/AccountRepo.java
+++ src/main/java/com/brotech/Auth/repo/AccountRepo.java
@@ -4,7 +4,9 @@ import com.brotech.Auth.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepo extends JpaRepository<Account,Long> {
-
+    Optional<Account> findByEmail(String email);
 }
