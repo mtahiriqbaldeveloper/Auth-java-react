4606202e8441e0c2e0791315315931c41ade7b11
Tahir Iqbal <tahiriqbaldeveloper@gmail.com>
2023-09-23 02:34:28 +0200

    login post request added

--- src/main/java/com/brotech/Auth/controller/AuthController.java
+++ src/main/java/com/brotech/Auth/controller/AuthController.java
@@ -1,13 +1,20 @@
 package com.brotech.Auth.controller;
 
 
+
+import com.brotech.Auth.JWT.JWTUtils;
+import com.brotech.Auth.common.payload.Login;
 import com.brotech.Auth.entity.Account;
 import com.brotech.Auth.service.AccountService;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @Controller
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping(path = "/v1/auth/")
 @CrossOrigin(origins = "http://localhost:3000/")
 public class AuthController {
